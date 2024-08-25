3f794fd03000f1ef8253a4a56a89ff7d89489754
ortwic <ohc84@gmx.de>
2024-08-25 08:48:09 +0200

    tuning logo and animation

--- apps/random-spa/src/App.svelte
+++ apps/random-spa/src/App.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   import { t } from "svelte-i18n";
   import Router, { link } from "svelte-spa-router";
-  import logo from "/logo.svg";
+  import logoSrc from "/logo.svg";
   import Adsense from "./lib/ad/Adsense.svelte";
   import Start from './routes/Start.svelte';
   import Random from './routes/Random.svelte';
   import Page from "./routes/Page.svelte";
   import NotFound from './routes/NotFound.svelte';
   import { setupI18n } from './lib/i18n';
+    import { onMount } from "svelte";
 
   const routes = {
     '/': Start,
@@ -15,6 +16,18 @@
     '/p/:path': Page,
     '*': NotFound
   };
+
+  let logoImg: HTMLImageElement;
+
+  onMount(animateLogo);
+
+  function animateLogo() {
+    if (logoImg?.classList) {
+      logoImg.classList.toggle('shake-ani');
+      setTimeout(() => logoImg.classList.remove('shake-ani'), 1600);
+      setTimeout(animateLogo, Math.random() * 4000 + 8000);
+    }
+  }
 </script>
 
 <svelte:head>
@@ -23,7 +36,9 @@
 </svelte:head>
 
 <header title="Shaking it all up!">
-  <a use:link href="/"><img src={logo} width="100" alt="Shaking it all up!" /></a>
+  <a use:link href="/" class="logo" title="Home">
+    <img class="shake shake-ani" bind:this={logoImg} src={logoSrc} width="60" alt="Shaking it all up!" />
+  </a>
   <h1>
     Randomizer
   </h1>
@@ -62,6 +77,33 @@
       flex-wrap: wrap;
       justify-content: center;
       align-items: center;
+  }
+
+  .logo {
+    transition: transform .2s ease-in-out;
+    transform-origin: 60% 80%;
+    margin: 0 1em;
+  }
+
+  .logo:hover {
+    transform: rotate(18deg);
+  }
+
+  .shake {
+    transition: transform .2s linear;
+    transform-origin: 70% 90%;
+  }
+
+  .shake:hover, .shake-ani {
+    animation: shake .5s infinite;
+  }
+
+  @keyframes shake {
+    0%, 100% { transform: rotate(0deg); }
+    10%, 30% { transform: rotate(3deg); }
+    40%, 50% { transform: rotate(-3deg); }
+    60%, 80% { transform: rotate(3deg); }
+    90% { transform: rotate(-3deg); }
   }
 
   footer {
