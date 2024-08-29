4e049743bbe49c9681e7417e98f7edbcde8e6fdc
ortwic <ohc84@gmx.de>
2024-08-29 20:20:00 +0200

    ux swiping indication bg gradient

--- apps/random-spa/src/routes/Random.svelte
+++ apps/random-spa/src/routes/Random.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import { t } from "svelte-i18n";
     import { derived } from "svelte/store";
-    import { fly } from "svelte/transition";
+    import { fly, fade } from "svelte/transition";
+    import { cubicInOut } from "svelte/easing";
     import { swipe } from 'svelte-gestures';
     import { pop, push } from "svelte-spa-router";
     import { gameStore } from "../lib/firebase/game.store";
@@ -16,6 +18,27 @@
         path?: string,
         level?: number
     } = {};
+    let indicateSwipeStart = true;
+    let background: string;
+
+    onMount(() => {
+        document.addEventListener('keydown', (e) => {
+            switch (e.key) {
+                case 'ArrowUp':
+                case 'ArrowLeft':
+                    e.preventDefault();
+                    prev();
+                    break;
+                case 'ArrowDown':
+                case 'ArrowRight':
+                    e.preventDefault();
+                    next();
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
 
     const config = derived(gameStore.documents, (games) => games.find((g) => g.id === params.path));
     const levels = derived(config, getLevels);
@@ -40,15 +63,33 @@
     }
 
     function prev() {
-        move.left();
-        randomIndex.prev();
-        pop();
+        if ($entry) {
+            move.left();
+            randomIndex.prev();
+            pop();
+        }
     }
 
     function next() {
-        move.right();
-        randomIndex.next();
-        push(`/r/${$config.id}/${$entry.level + 1}/${$randomIndex}.${$documents.length}`);
+        if ($entry) {
+            move.right();
+            randomIndex.next();
+            push(`/r/${$config.id}/${$entry.level + 1}/${$randomIndex}.${$documents.length}`);
+        }
+    }
+
+    function swipeStart({ x, y }) {
+        swipeMove({ x, y });
+        setTimeout(swipeEnd, 900);
+        indicateSwipeStart = true;
+    }
+
+    function swipeMove({ x, y }) {
+        background = `radial-gradient(circle at ${x}px ${y}px, #646cff20, transparent)`;    
+    }
+
+    function swipeEnd() {
+        indicateSwipeStart = false;
     }
 
     function swiped({ direction }) {
@@ -60,9 +101,27 @@
     }
 </script>
 
+<!-- Todo remove use:swipe on destroy -->
+<!-- <svelte:body
+  use:swipe={{ timeframe: 500, minSwipeDistance: 50 }} 
+  on:swipedown={(e) => swipeStart(e.detail)}
+  on:swipemove={(e) => swipeMove(e.detail)}
+  on:swipeup={(e) => swipeEnd()}
+  on:swipe={(e) => swiped(e.detail)}/> -->
+
+{#if indicateSwipeStart}
+<div class="backdrop" style:background
+  in:fade={{ duration: 200, easing: cubicInOut }}
+  out:fade={{ duration: 400, easing: cubicInOut }}></div>
+{/if}
+
 {#key $move}
 <section class="absolute grid" in:fly={move.in} out:fly={move.out}
-    use:swipe={{ timeframe: 500, minSwipeDistance: 50 }} on:swipe={(e) => swiped(e.detail)}>
+    use:swipe={{ timeframe: 500, minSwipeDistance: 50 }} 
+    on:swipedown={(e) => swipeStart(e.detail)}
+    on:swipemove={(e) => swipeMove(e.detail)}
+    on:swipeup={(e) => swipeEnd()}
+    on:swipe={(e) => swiped(e.detail)}>
     <button on:click={prev}>&lt;&lt; { $t('random.prev') }</button>
     <button on:click={next}>{ $t('random.next') } &gt;&gt;</button>
     <span class="col-2">
@@ -87,5 +146,14 @@
 
     .col-2 {
         grid-column: span 2;
+    }
+
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
     }
 </style>
