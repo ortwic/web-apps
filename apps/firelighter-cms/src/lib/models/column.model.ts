5e0439cb1b28490f6dbb53a19e88bd3d8226e718
ortwic <ohc84@gmx.de>
2024-09-04 13:10:14 +0200

    [fl-cms] removed firelighter-cms

--- apps/firelighter-cms/src/lib/models/column.model.ts
+++
@@ -1,9 +0,0 @@
-import type { MenuObject, CellComponent, MenuSeparator } from "tabulator-tables";
-
-export interface ColumnOptions<T> {
-    idField: string & keyof T;
-    maxWidth?: number;
-    maxHeight?: number;
-    actions?: Array<MenuObject<CellComponent> | MenuSeparator>;
-    updateHandler?: (data: T) => void;
-}
--- apps/firelighter-cms/src/lib/packages/schema_inference/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./collection_builder";
-export * from "./util";
