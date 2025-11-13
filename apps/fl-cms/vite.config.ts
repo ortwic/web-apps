9c848322f2f0ff00b9a4e2d361ad12e3505331f2
Ortwin <ohc84@gmx.de>
2025-11-13 18:16:38 +0100

    [fl-cms] PWA support

--- apps/fl-cms/vite.config.ts
+++ apps/fl-cms/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,11 +14,65 @@ export default defineConfig({
       },
     },
   },
-  plugins: [svelte({
-    compilerOptions: {
-      customElement: true
-    }
-  })],
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true
+      }
+    }),
+    VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: [],
+        manifest: {
+            name: 'Fl-CMS',
+            short_name: 'Fl-CMS',
+            start_url: '/',
+            description: 'Simple Firebase Content Management System.',
+            display: 'fullscreen',
+            background_color: '#303030',
+            theme_color: '#80c0ff',
+            lang: 'en',
+            scope: '/',
+            categories: ['CMS', 'productivity', 'utilities'],
+            icons: [
+                {
+                    src: 'icons/icon-192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                    purpose: 'any',
+                },
+                {
+                    src: 'icons/icon-512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                },
+                {
+                    src: 'icons/icon-192-mono.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                    purpose: 'monochrome',
+                },
+                {
+                    src: 'icons/icon-512-mono.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'monochrome',
+                },
+            ],
+            screenshots: [
+            ],
+            display_override: ['window-controls-overlay'],
+            orientation: 'any',
+        },
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
+            globIgnores: ['**/node_modules/**/*'],
+        },
+        devOptions: { enabled: true },
+        selfDestroying: false,
+    }),
+],
   server: {
     port: 5000
   }
